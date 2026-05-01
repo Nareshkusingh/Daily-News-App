4e644e8453e228efcda15aa3043acea245a6fc17
Naresh kumar <nareshkusingh09@gmail.com>
2026-05-01 22:31:51 +0530

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class NewsApp {
     constructor() {
-        this.API_KEY = "c40bf4127e09416094fdfd4fa901be3b";
-        this.BASE_URL = "https://newsapi.org/v2";
-
+         this.API_KEY = "c40bf4127e09416094fdfd4fa901be3b";
+        // this.BASE_URL = "https://newsapi.org/v2";
+        this.BASE_URL = `${this.BASE_URL}/top-headlines?country=us&max=10&token=${this.API_KEY}`;
         this.state = {
             category: "general",
             page: 1,
